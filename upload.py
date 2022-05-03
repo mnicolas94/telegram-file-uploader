6f1f3f503bfd6d49a337963a53707490c92d3d2b
mnicolas94 <miguelcok27@gmail.com>
2022-05-03 14:38:52 -0400

    FIX: test to get session string

--- upload.py
+++ upload.py
@@ -31,7 +31,24 @@ if __name__ == '__main__':
     file_path = args.file_path
     message = args.message
 
-    app = Client(session_name, session_string=session_hash, api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-    app.run(send_document(app, chat_id, file_path, message))
+    # app = Client(session_name, session_string=session_hash, api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+    # app.run(send_document(app, chat_id, file_path, message))
+
+    app = Client(session_name, in_memory=True, api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+    messages = []
+    @app.on_message()
+    async def my_handler(client, message):
+        # print(client)
+        print(message)
+        messages.append(message)
+        if len(messages) >= 2:
+            s = await app.export_session_string()
+            raise Exception(s)
+
+    app.run()
+
+
+
 
 
